e7ba97f55338cac9269c036a2fe7bc45cea3914f
Nikhil Arya <nikhilarya3108202@gmail.com>
2025-08-28 17:37:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 function insert_Row() {
     //Write your code here
-	let table = document.querySelector("#sampleTable");
-	let th = document.createElement("tr");
-	let tr1 = document.createElement("td");
-	tr1.innerText = "New Cell1";
-	let tr2 = document.createElement("td");
-	tr2.innerText = "New Cell2";
+	 let table = document.querySelector("#sampleTable");
 
-	th.append(tr1);
-	th.append(tr2);
+    // make sure we target the tbody (or the table itself if no tbody exists)
+    let tbody = table.querySelector("tbody") || table;
 
-	table.prepend(th);
-  
+    let tr = document.createElement("tr");
+
+    let td1 = document.createElement("td");
+    td1.innerText = "New Cell1";
+
+    let td2 = document.createElement("td");
+    td2.innerText = "New Cell2";
+
+    tr.append(td1, td2);
+
+    // insert at the top
+    tbody.insertBefore(tr, tbody.firstChild);
 }
