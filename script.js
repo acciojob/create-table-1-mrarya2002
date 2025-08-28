82963848b49795d9745993de639b3406cdca3a8c
Nikhil Arya <nikhilarya3108202@gmail.com>
2025-08-28 17:31:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function insert_Row() {
     //Write your code here
-  
+	let table = document.querySelector("#sampleTable");
+	let th = document.createElement("tr");
+	let tr1 = document.createElement("td");
+	tr1.innerText = "New Cell1";
+	let tr2 = document.createElement("td");
+	tr2.innerText = "New Cell2";
+
+	th.append(tr1);
+	th.append(tr2);
+
+	table.prepend(th);
   
 }
